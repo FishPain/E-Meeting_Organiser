cc7e86922fcadfbd4a47fb3169c0f54f82a7b598
Dylan Kok <tenkotofu@gmail.com>
2020-11-01 03:15:04 +0800

    Convert Pure JS to jquery

--- js/app.js
+++ js/app.js
@@ -1,40 +1,37 @@
-    // class containing all the links
-    var grps = document.getElementsByClassName("grp");
-    var close = document.getElementsByClassName("close");
-
-    // Check for existing links in local storage
-      if (localStorage.getItem("ZoomLinkArray") == null){
-        var links_array = [];
-      }
-      else{
-        var links_array = JSON.parse(localStorage.getItem("ZoomLinkArray"));
-      }
+// Check for existing links in local storage
+if (localStorage.getItem("ZoomLinkArray") == null) {
+    var links_array = [];
+} else {
+    var links_array = JSON.parse(localStorage.getItem("ZoomLinkArray"));
+}
 
 
-      // adding link and name to the website
-      function addLink(){
-        var passCheck = true;
-        var link = document.getElementById("zoomLink_input").value;
-        var name = document.getElementById("zoomLink_name").value;
-        // add the info into an object
-        if (link != "" && name != "") {
-          for (var i = 0; i < links_array.length; i++) {
+$(document).ready(displayLink());
+
+// adding link and name to the website
+const addLink = () => {
+    var passCheck = true;
+    var link = $("#zoomLink_input").val();
+    var name = $("#zoomLink_name").val();
+    // add the info into an object
+    if (link != "" && name != "") {
+        for (var i = 0; i < links_array.length; i++) {
             var nameCheck = links_array[i].Name;
             var linkCheck = links_array[i].Link;
-            if (name == nameCheck){
-              alert("This name already exist!")
-              passCheck = false;
+            if (name == nameCheck) {
+                alert("This name already exist!")
+                passCheck = false;
             }
-            else if (link == linkCheck){
-              alert("This link already exist!")
-              passCheck = false;
+            else if (link == linkCheck) {
+                alert("This link already exist!")
+                passCheck = false;
             }
-          }
-          if (passCheck) {
+        }
+        if (passCheck) {
             var link_obj =
             {
-              "Name": '',
-              "Link": ''
+                "Name": '',
+                "Link": ''
             };
 
             link_obj["Name"] = name;
@@ -44,94 +41,76 @@
             links_array.push(link_obj);
 
             // adds array to local storage
-            localStorage.setItem("ZoomLinkArray",JSON.stringify(links_array));
-          }
+            localStorage.setItem("ZoomLinkArray", JSON.stringify(links_array));
         }
-        else {
-          alert("Please enter both Zoom Link and Course Name")
-        }
+    }
+    else {
+        alert("Please enter both Zoom Link and Course Name")
+    }
+    location.reload();
+}
+
+
+function displayLink() {
+    // delete all link
+    for (var i = 0; i < links_array.length; i++) {
+        var name = links_array[i].Name;
+        var link = links_array[i].Link;
+
+        var element = `
+        <div class="grp">
+        <li>
+            <a href="${link}" target="_blank">${link}</a>
+            <button type="button" class="close" arialabel="Close" id="${i}"
+                onclick="removeLink(this.id)" style="display: none;"><span ariahidden="true">x</span>
+            </button>
+        </li>
+        </div>`
+        
+        $("#display").append(element);
+    }
+}
+
+let editButtonState = false
+
+$("#edit").click(() => {
+    if (editButtonState === false) {
+        editButtonState = true
+        $("#edit").text("Save")
+
+        $(".grp").each(function (i) {
+            // change link border to orange
+            $(this).css('border-color', "orange");
+            // Display close icon
+            $(".close").show();
+        });
+    } else {
+        editButtonState = false
+        $("#edit").text("Edit")
+
+        $(".grp").each(function (i) {
+            // change link border to orange
+            $(this).css('border-color', "black");
+            // Display close icon
+            $(".close").hide();
+        });
         location.reload();
-      }
+    }
+})
 
 
-      function displayLink() {
-        // delete all link
-        for (var i = 0; i < links_array.length; i++) {
-          var name = links_array[i].Name;
-          var link = links_array[i].Link;
+const removeLink = (idValue) => {
+    // console.log(idValue);
+    links_array.splice(idValue, 1);
+    localStorage.setItem("ZoomLinkArray", JSON.stringify(links_array));
+    $(".grp")[idValue].remove();
 
-          var textNode = document.createTextNode(name);
-          var textNode_remove = document.createTextNode("Remove Link");
-
-          var div = document.createElement("div");
-          div.setAttribute("class", "grp");
-          var li = document.createElement("li");
-          // Set href link
-          var a = document.createElement("a");
-          a.setAttribute("href", link);
-          a.setAttribute("target", "_blank")
-          a.appendChild(textNode);
-          li.appendChild(a);
-          // Create close button
-          var span = document.createElement("span");
-          span.setAttribute("ariaHidden", "true");
-          span.innerText = "x"
-          var btn = document.createElement("button");
-          btn.setAttribute("type", "button");
-          btn.setAttribute("class", "close");
-          btn.setAttribute("ariaLabel", "Close");
-          btn.setAttribute("id", i);
-          btn.setAttribute("onclick", "removeLink(this.id)")
-          btn.appendChild(span);
-          // button hidden by default
-          btn.style.display = "none";
-          li.appendChild(btn);
-          div.appendChild(li);
-          var display = document.getElementById("display").appendChild(div);
-        }
-      }
+    $(".grp").each(function (i) {
+        $(".grp")[i]
+    });
+}
 
 
-      function edit(){
-        // Edit the links
-        if (document.getElementById("edit").innerText == "Edit"){
-          // onclick change button text to "save"
-          document.getElementById("edit").innerText = "Save";
-          for(var i = 0; i < grps.length; i++){
-            // change link border to orange
-            grps[i].style.borderColor = "orange";
-            // Display close icon
-            close[i].style.display = "block";
-          }
-        }
-        // Save the edits
-        else{
-          // onclick change button text to "Edit"
-          document.getElementById("edit").innerText = "Edit";
-          for(var i = 0; i < grps.length; i++){
-            // change link border to orange
-            grps[i].style.borderColor = "black";
-            // Display close icon
-            close[i].style.display = "none";
-          }
-          location.reload();
-        }
-      }
+const updateLink = () => {
 
-
-      function removeLink(idValue){
-        console.log(idValue);
-        links_array.splice(idValue, 1);
-        localStorage.setItem("ZoomLinkArray", JSON.stringify(links_array));
-        grps[idValue].remove();
-        for (var i = 0; i<grps.length; i++){
-          grps[i]
-        }
-      }
-
-
-      function updateLink(){
-
-      }
-
-      displayLink();
+}
